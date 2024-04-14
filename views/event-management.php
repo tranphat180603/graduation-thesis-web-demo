2f4206e45c667432c9f10edfc2106d5149de77c5
myduyen2212 <nguyenhoangmyduyen213@gmail.com>
2024-04-14 08:14:51 +0700

    event commit

--- views/event-management.php
+++ views/event-management.php
@@ -346,9 +346,27 @@
             <div class="input">
               <select name='event_preferential_rate'>
                 <option value='0'>Chọn tỷ lệ ưu đãi</option>
-                <option value='5'>0%</option>
+                <option value='0'>0%</option>
                 <option value='5'>5%</option>
                 <option value='10'>10%</option>
+                <option value='15'>15%</option>
+                <option value='20'>20%</option>
+                <option value='25'>25%</option>
+                <option value='30'>30%</option>
+                <option value='35'>35%</option>
+                <option value='40'>40%</option>
+                <option value='45'>45%</option>
+                <option value='50'>50%</option>
+                <option value='55'>55%</option>
+                <option value='60'>60%</option>
+                <option value='65'>65%</option>
+                <option value='70'>70%</option>
+                <option value='75'>75%</option>
+                <option value='80'>80%</option>
+                <option value='85'>85%</option>
+                <option value='90'>90%</option>
+                <option value='95'>95%</option>
+                <option value='100'>100%</option>
               </select>
             </div>
           </div>
@@ -474,6 +492,24 @@
                 <option value='0'>0%</option>
                 <option value='5'>5%</option>
                 <option value='10'>10%</option>
+                <option value='15'>15%</option>
+                <option value='20'>20%</option>
+                <option value='25'>25%</option>
+                <option value='30'>30%</option>
+                <option value='35'>35%</option>
+                <option value='40'>40%</option>
+                <option value='45'>45%</option>
+                <option value='50'>50%</option>
+                <option value='55'>55%</option>
+                <option value='60'>60%</option>
+                <option value='65'>65%</option>
+                <option value='70'>70%</option>
+                <option value='75'>75%</option>
+                <option value='80'>80%</option>
+                <option value='85'>85%</option>
+                <option value='90'>90%</option>
+                <option value='95'>95%</option>
+                <option value='100'>100%</option>
               </select>
             </div>
           </div>

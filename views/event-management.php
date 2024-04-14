0501a340dd85f00ee1de7695286b8a21925d4c71
tranphat180603 <you@example.com>
2024-04-14 08:55:31 +0700

    14/04

--- views/event-management.php
+++ views/event-management.php
@@ -346,9 +346,27 @@
             <div class="input">
               <select name='event_preferential_rate'>
                 <option value='0'>Chọn tỷ lệ ưu đãi</option>
-                <option value='5'>0%</option>
+                <option value='0'>0%</option>
                 <option value='5'>5%</option>
                 <option value='10'>10%</option>
+                <option value='15'>15%</option>
+                <option value='20'>20%</option>
+                <option value='25'>25%</option>
+                <option value='30'>30%</option>
+                <option value='35'>35%</option>
+                <option value='40'>40%</option>
+                <option value='45'>45%</option>
+                <option value='50'>50%</option>
+                <option value='55'>55%</option>
+                <option value='60'>60%</option>
+                <option value='65'>65%</option>
+                <option value='70'>70%</option>
+                <option value='75'>75%</option>
+                <option value='80'>80%</option>
+                <option value='85'>85%</option>
+                <option value='90'>90%</option>
+                <option value='95'>95%</option>
+                <option value='100'>100%</option>
               </select>
             </div>
           </div>
@@ -471,9 +489,27 @@
             <div class="input">
               <select name='event_preferential_rate'>
                 <option value='0'>Chọn tỷ lệ ưu đãi</option>
-                <option value='5'>0%</option>
+                <option value='0'>0%</option>
                 <option value='5'>5%</option>
                 <option value='10'>10%</option>
+                <option value='15'>15%</option>
+                <option value='20'>20%</option>
+                <option value='25'>25%</option>
+                <option value='30'>30%</option>
+                <option value='35'>35%</option>
+                <option value='40'>40%</option>
+                <option value='45'>45%</option>
+                <option value='50'>50%</option>
+                <option value='55'>55%</option>
+                <option value='60'>60%</option>
+                <option value='65'>65%</option>
+                <option value='70'>70%</option>
+                <option value='75'>75%</option>
+                <option value='80'>80%</option>
+                <option value='85'>85%</option>
+                <option value='90'>90%</option>
+                <option value='95'>95%</option>
+                <option value='100'>100%</option>
               </select>
             </div>
           </div>
@@ -489,8 +525,8 @@
           <p>Trạng thái :</p>
             <div class="input" >
               <select name='event_state' required>
-                <option value='còn hạn'>Còn hạn</option>
-                <option value='hết hạn'>Hết hạn</option>
+                <option value='Còn hạn'>Còn hạn</option>
+                <option value='Hết hạn'>Hết hạn</option>
               </select>
             </div>         
           </div>
